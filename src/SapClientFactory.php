8326542ed3de9bc0bbd0f1094d2ec5a8696d79ef
Ignacio Cuadra <ignacio.cuadra.z@gmail.com>
2023-01-22 16:16:28 -0300

    fix: :ambulance: fix getenv names

--- src/SapClientFactory.php
+++ src/SapClientFactory.php
@@ -13,10 +13,10 @@ class SapClientFactory {
     $password = null
   ) {
     if (is_null($host)) $host = getenv("ICSAP_HOST");
-    if (is_null($port)) $port = getenv("ICSAP_HOST");
-    if (is_null($database)) $database = getenv("ICSAP_HOST");
-    if (is_null($username)) $username = getenv("ICSAP_HOST");
-    if (is_null($password)) $password = getenv("ICSAP_HOST");
+    if (is_null($port)) $port = getenv("ICSAP_PORT");
+    if (is_null($database)) $database = getenv("ICSAP_DATABASE");
+    if (is_null($username)) $username = getenv("ICSAP_USERNAME");
+    if (is_null($password)) $password = getenv("ICSAP_PASSWORD");
     return new SapClient($host, $port, $database, $username, $password);
   }  
 }
